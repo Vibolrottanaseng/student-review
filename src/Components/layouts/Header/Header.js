5f5989227455e40ebf852f5cd2e0aa9c1f01765a
Kaung Myat Htet <97383493+kaungmyat-htet@users.noreply.github.com>
2023-02-20 03:35:12 +0700

    add signup feature

--- src/Components/layouts/Header/Header.js
+++ src/Components/layouts/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import classes from './Header.module.css'
 import BrowseCourse from "./browse-course";
+import { Link } from "react-router-dom";
 
 
 const Header = props => {
@@ -8,6 +9,13 @@ const Header = props => {
         <header className={classes.header}>
             <h1>Review</h1>
             <BrowseCourse />
+            <nav>
+                <Link
+                    href='/'
+                    to='/SignUp'>
+                    Register
+                </Link>
+            </nav>
         </header>
         {/* <div className={classes['image-container']}>
             <img className={classes.image} src={abac} alt="abac" />
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,155 +1,253 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import FormControl from '@mui/material/FormControl'
-import { InputLabel } from '@mui/material';
-import NativeSelect from '@mui/material/NativeSelect'
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
+import * as React from "react";
+import { useState } from "react";
+import axiosInstance from "./axios";
+import { useNavigate } from 'react-router-dom';
+// Material UI
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import FormControl from "@mui/material/FormControl";
+import { InputLabel } from "@mui/material";
+import NativeSelect from "@mui/material/NativeSelect";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import Link from "@mui/material/Link";
+import Alert from '@mui/material/Alert';
 
 const theme = createTheme();
 
 export default function SignUp() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
-    };
+    const navigate = useNavigate();
 
-    const [Faculty, setFaculty] = React.useState('');
+  const initialFormData = Object.freeze({
+    firstName: "",
+    lastName: "",
+    studentId: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    faculty: ""
+  });
 
-    const handleChange = (event) => {
-        setFaculty(event.target.value);
-    }
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'error.main' }}>
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign up
-                    </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="lastName"
-                                    label="First Name"
-                                    // type="last"
-                                    type="text"
-                                    id="lastName"
-                                    autoComplete="family-name"
-                                    variant='standard'
-                                    autoFocus
-                                    color="error"
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="lastName"
-                                    label="Last Name"
-                                    // type="last"
-                                    type="text"
-                                    id="lastName"
-                                    autoComplete="family-name"
-                                    variant='standard'
-                                    autoFocus
-                                    color="error"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="id"
-                                    label="ID"
-                                    type="text"
-                                    id="email"
-                                    autoComplete="id"
-                                    variant='standard'
-                                    autoFocus
-                                    color="error"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControl required variant="standard" sx={{ minWidth: 420 }}>
-                                    <InputLabel id="demo-simple-select-standard-label">Faculty</InputLabel>
-                                    <Select
-                                        labelId="demo-simple-select-standard-label"
-                                        id="demo-simple-select-standard"
-                                        value={Faculty}
-                                        onChange={handleChange}
-                                        label="Faculty"
-                                        color="error"
-                                    >
-                                        <MenuItem value={'MSME'}>School of Management and Economics</MenuItem>
-                                        <MenuItem value={'AR'}>School of Architechture and Desing</MenuItem>
-                                        <MenuItem value={'VMS'}>School of Science and Technlogy</MenuItem>
-                                        <MenuItem value={'CA'}>School of Communication Art</MenuItem>
-                                        <MenuItem value={'NS'}>School of Nursing Science</MenuItem>
-                                        <MenuItem value={'VME'}>School of Engineering</MenuItem>
-                                        <MenuItem value={'BA'}>School of Business</MenuItem>
-                                        <MenuItem value={'ART'}>School of Art</MenuItem>
-                                        <MenuItem value={'MU'}>School of Music</MenuItem>
-                                        <MenuItem value={'LA'}>School of Law</MenuItem>
-                                    </Select>
-                                </FormControl>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                    variant="standard"
-                                    color="error"
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="error"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign Up
-                        </Button>
+  const [formData, updateFormData] = useState(initialFormData);
 
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider >
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    console.log(Faculty);
+
+    axiosInstance
+      .post("register/", {
+        email: formData.email,
+        password: formData.password,
+        password2: formData.confirmPassword,
+        first_name: formData.firstName,
+        last_name: formData.lastName,
+        student_id: formData.studentId,
+        faculty: "1",
+      })
+      .then((res) => {
+        navigate("/SignIn");
+        if (res.status == 201) {
+            alert("Account Created Successfully.")
+        }
+        console.log(res);
+        console.log(res.data);
+      }).catch((error) => {
+        if (error.response) {
+            console.log(error.response.data);
+            alert(JSON.stringify(error.response.data));
+        }
+      });
+  };
+
+  const [Faculty, setFaculty] = React.useState("");
+
+  const handleFacultyChange = (event) => {
+    setFaculty(event.target.value);
+  };
+
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "error.main" }}></Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            // onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="firstName"
+                  label="First Name"
+                  id="firstName"
+                  autoComplete="firstname"
+                  variant="standard"
+                  autoFocus
+                  color="error"
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="lastName"
+                  label="Last Name"
+                  id="lastName"
+                  autoComplete="Family Name"
+                  variant="standard"
+                  autoFocus
+                  color="error"
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="studentId"
+                  label="Student ID Number"
+                  type="text"
+                  id="studentId"
+                  autoComplete="email"
+                  variant="standard"
+                  autoFocus
+                  color="error"
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl required variant="standard" sx={{ minWidth: 420 }}>
+                  <InputLabel id="demo-simple-select-standard-label">
+                    Faculty
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={Faculty}
+                    onChange={handleFacultyChange}
+                    label="Faculty"
+                    color="error"
+                  >
+                    <MenuItem value={"MSME"}>
+                      School of Management and Economics
+                    </MenuItem>
+                    <MenuItem value={"AR"}>
+                      School of Architechture and Desing
+                    </MenuItem>
+                    <MenuItem value={"VMS"}>
+                      School of Science and Technlogy
+                    </MenuItem>
+                    <MenuItem value={"CA"}>
+                      School of Communication Art
+                    </MenuItem>
+                    <MenuItem value={"NS"}>School of Nursing Science</MenuItem>
+                    <MenuItem value={"VME"}>School of Engineering</MenuItem>
+                    <MenuItem value={"BA"}>School of Business</MenuItem>
+                    <MenuItem value={"ART"}>School of Art</MenuItem>
+                    <MenuItem value={"MU"}>School of Music</MenuItem>
+                    <MenuItem value={"LA"}>School of Law</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="email"
+                  label="AU Email Address"
+                  type="email"
+                  id="email"
+                  autoComplete="email"
+                  variant="standard"
+                  color="error"
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="password"
+                  variant="standard"
+                  color="error"
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  type="password"
+                  id="confirmPassword"
+                  autoComplete="confirm-password"
+                  variant="standard"
+                  color="error"
+                  onChange={handleChange}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="error"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleSubmit}
+            >
+              Sign Up
+            </Button>
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
